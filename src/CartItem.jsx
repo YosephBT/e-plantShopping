2b5ae46efd4b49b45064ef491f53601e01ce9b40
YosephBT <yosephbelay.t@gmail.com>
2025-03-08 23:59:14 +0300

    updated CartItem reducers and the product list component

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,15 +9,15 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
-    let total=0;
-    cart.forEach((item)=>{
-      total+=(parseFloat(item.cost.substring(1))*item.quantity)
+    let totalAmount = 0;
+    cart.forEach((item) => {
+      totalAmount += (parseFloat(item.cost.substring(1)) * item.quantity)
     })
-    return total;
+    return totalAmount;
   };
-
+  const totalAmount=calculateTotalAmount();
   const handleContinueShopping = (e) => {
-      onContinueShopping(e);
+    onContinueShopping(e);
   };
 
   const handleCheckoutShopping = (e) => {
@@ -25,25 +25,25 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleIncrement = (item) => {
-         dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-    if(item.quantity>1)
+    if (item.quantity > 1)
       dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
     else
       dispatch(removeItem(item.name));
-    
+
   };
 
   const handleRemove = (item) => {
     dispatch(removeItem(item));
   };
 
-  // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let totalCost = (parseFloat(item.cost.substring(1)) * item.quantity)
+    return totalCost;
   };
-
   return (
     <div className="cart-container">
       <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
@@ -65,7 +65,7 @@ const CartItem = ({ onContinueShopping }) => {
           </div>
         ))}
       </div>
-      <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
+      <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'>{totalAmount}</div>
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
